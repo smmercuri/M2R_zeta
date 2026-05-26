022f28add987c76cd635f2a8da366ccdf49e34c5
Salvatore Mercuri <salvatore.mercuri@imperial.ac.uk>
2026-05-26 11:08:09 +0100

    draft

---
+++ zeta_value.py
@@ -0,0 +1,24 @@
+from sage.all import * 
+import numpy as np
+
+def bernoulli_number(n): 
+    return 0 
+
+def partial_zeta(s, N):
+    return 0 
+
+def integral_term(s, N):
+    return 0 
+
+def correction_term(s, N):
+    return 1 / (2 * N ** s) 
+
+def bernoulli_term(i, N, s):
+    return 0
+
+def bernoulli_sum(N, s, v):
+    return 0 
+
+def zeta_euler_maclaurin(s, N, v):
+    return 0 
+
